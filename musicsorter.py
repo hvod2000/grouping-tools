c139e2383f1518db75995baa8f34f4a48d782a3a
Uladzislau Khamkou <kham@tuta.io>
2022-01-14 16:17:32 +0300

    musicsorter: refactoring

--- musicsorter.py
+++ musicsorter.py
@@ -1,5 +1,5 @@
 from collections import namedtuple, defaultdict
-from pathlib3x import Path # from pathlib import Path
+from pathlib3x import Path  # from pathlib import Path
 import re
 import wikipedia
 
@@ -38,27 +38,27 @@ class Gallery:
     def update_authors_info(self):
         for author, info in list(self.authors.items()):
             self.authors[author] = info or get_info_from_wikipedia(author)
-            self.save()
+            authors2tsv(self.authors, self.content_dir)
+        self.save()
 
     def save(self):
-        table = [AUTHORS_TSV_HEADER]
+        authors2tsv(self.authors, self.content_dir)
+        self.save_genres()
+
+    def save_genres(self):
         genres = defaultdict(set)
         for author, info in sorted(self.authors.items()):
             if info:
-                table.append("\t".join([author, ", ".join(info.genres)]))
                 for g in info.genres:
                     genres[g].add(author)
-        with (self.content_dir / "authors.tsv.part").open("wt") as f:
-            f.write("\n".join(table))
-        the_dir = self.content_dir
-        (the_dir / "authors.tsv.part").rename(the_dir / "authors.tsv")
         linkp = Path("..") / "all"
         for genre, authors in genres.items():
+            print(f"save genre {genre}:{authors}")
             if (self.content_dir / genre).is_dir():
                 (self.content_dir / genre).rmtree()
             (self.content_dir / genre).mkdir()
             for author in authors:
-                (the_dir / genre / author).symlink_to(linkp / author)
+                (self.content_dir / genre / author).symlink_to(linkp / author)
 
 
 def tcv2authors(table_path):
@@ -66,6 +66,16 @@ def tcv2authors(table_path):
     assert table[0] == AUTHORS_TSV_HEADER
     data = (row.split("\t") for row in table[1:])
     return {n: MusicAuthorInfo(n, g.split(", ") if g else []) for n, g in data}
+
+
+def authors2tsv(authors, the_dir):
+    table = [AUTHORS_TSV_HEADER]
+    for author, info in sorted(authors.items()):
+        if info:
+            table.append("\t".join([author, ", ".join(info.genres)]))
+    with (the_dir / "authors.tsv.part").open("wt") as f:
+        f.write("\n".join(table))
+    (the_dir / "authors.tsv.part").rename(the_dir / "authors.tsv")
 
 
 def get_info_from_wikipedia(name):
@@ -84,13 +94,14 @@ def get_info_from_wikipedia(name):
     except ValueError:
         return MusicAuthorInfo(name, [])
     genres = re.findall(r'title="[^"]*"', html[i:j])
-    genres = [g.split("=")[1].strip('"').replace('/', '-') for g in genres]
+    genres = [g.split("=")[1].strip('"').replace("/", "-") for g in genres]
     print(page.title, genres)
     return MusicAuthorInfo(page.title, genres)
 
 
 if __name__ == "__main__":
     import sys
+
     g = Gallery.from_mess_without_any_info(sys.argv[1])
     g.update_list_of_authors()
     g.update_authors_info()
